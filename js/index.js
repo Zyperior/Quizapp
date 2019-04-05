b68b8825f7bc1fcb007102851c45b10f56814fd3
Andreas <albihn.andreas@gmail.com>
2019-04-05 16:52:13 +0200

    Added commentary to the code + minor fixes

--- js/index.js
+++ js/index.js
@@ -1,40 +1,62 @@
+/*
+ * Author: Andreas Albihn
+ * Project: QuizApp, ITHS 2019
+ * Date: 2019-04-05
+ */
+
 let errorMessage = document.getElementById("errorMessage");
 let numberOfPlayers = document.getElementById("nrPlayers");
 let createButton = document.getElementById("createQuiz");
-
+let categorySelector = document.getElementById("category");
+let difficultySelector = document.getElementById("difficulty");
 createButton.addEventListener("click", openNewQuiz);
 
-function openNewQuiz(){
+/*
+Functionality for Create Quiz button click-event
+ */
+async function openNewQuiz(){
 
+    //Initial reset
     errorMessage.className = "errorMessage invisible";
-
     localStorage.clear();
 
     getQuizData().then(function (QuizData) {
 
         controlQuizData(QuizData).then(function(){
 
+            //Store data for use in quiz
             localStorage.setItem("numberOfPlayers", numberOfPlayers.value);
             localStorage.setItem("quizData", QuizData);
             openNewWindow();
 
-        }, function (){
+        }, function (Error){
 
+            console.log(Error.message);
             errorMessage.className = "errorMessage show"
 
         });
-
-    }, function (ErrorMessage) {
-
-        console.log(ErrorMessage);
 
     });
 
 }
 
-getQuizData = function(){
+function getQuizData(){
 
     return new Promise((resolve) => {
+
+        //Set request parameters
+        let categoryValue = categorySelector[categorySelector.selectedIndex].value;
+        let difficulty = difficultySelector.value;
+        let url = "https://opentdb.com/api.php?amount=10";
+
+        //Add parameters to url if necessary
+        if(categoryValue!=="any"){
+            url = url + "&category=" + categoryValue
+        }
+
+        if(difficulty!=="any"){
+            url = url + "&difficulty=" + difficulty
+        }
 
         let xhttp = new XMLHttpRequest();
 
@@ -51,24 +73,31 @@ getQuizData = function(){
 
         };
 
-        xhttp.open("GET", "https://opentdb.com/api.php?amount=8");
+        xhttp.open("GET", url);
         xhttp.send();
 
     });
-};
+}
 
 function controlQuizData(data){
 
     return new Promise(((resolve, reject) => {
 
+        //Parse response data to JSON array
         let dataJSON = JSON.parse(data);
 
         setTimeout(function(){
 
+            //Control if array is lower than 10
             if(dataJSON.results.length<10){
 
-                reject();
+                //If so, reject with error message
+                reject(new Error("Request response must contain >9 questions, response: " +
+                    dataJSON.results.length +
+                    " questions"));
             }
+
+            //else resolve
             resolve();
 
         },500)
@@ -77,15 +106,15 @@ function controlQuizData(data){
 
 }
 
-openNewWindow = function () {
+function openNewWindow() {
 
     setTimeout(()=>{
 
         window.open("./quiz.html");
 
-    }, 1000);
+    }, 500);
 
-};
+}
 
 
 
--- js/quiz.js
+++ js/quiz.js
@@ -1,3 +1,8 @@
+/*
+ * Author: Andreas Albihn
+ * Project: QuizApp, ITHS 2019
+ * Date: 2019-04-05
+ */
 
 let answerButtons;
 let quizDataJSON;
@@ -15,13 +20,16 @@ let progressGrid = document.getElementById("progressGrid");
 let questionNr = 0;
 let playerNr = 0;
 
-initializeQuiz = function(){
+function initializeQuiz(){
 
+    //Set quiz essential params from local storage
     numberOfPlayers = parseInt(localStorage.getItem("numberOfPlayers"));
     quizDataJSON = JSON.parse(localStorage.getItem("quizData"));
 
-    playerDataJSON = createAnswerJSON();
+    //Create a JSON array for the answer data for each player
+    playerDataJSON = createPlayerJSON();
 
+    //Set the appropriate amount of progress lines
     for(let i=0; i<4; i++){
         let progressLine = document.getElementById("progressLine" + i);
         if(i>=numberOfPlayers){
@@ -29,16 +37,20 @@ initializeQuiz = function(){
         }
     }
 
+    //Add initial eventlistener to lock answer button
     lockAnswerButton.addEventListener("click",lockAnswer);
+
+    //Set the first turn
     setTurn();
 
-};
+}
 
-initializeQuiz();
+function createPlayerJSON(){
 
-function createAnswerJSON(){
-
+    //Initial Json string
     let playersString = "{\"player\":[";
+
+    //Add answer strings to each player
     for(let i = 0; i<numberOfPlayers; i++){
         if(i===numberOfPlayers-1){
             playersString = playersString +
@@ -50,7 +62,7 @@ function createAnswerJSON(){
         }
     }
 
-
+    //final answer string
     playersString = playersString + "]}";
 
     return JSON.parse(playersString);
@@ -58,20 +70,20 @@ function createAnswerJSON(){
 
 
 function setTurn(){
-    console.log(questionNr);
-    if(questionNr>=10){
-        endQuiz();
+
+    //Check if last player and not completed
+    if(playerNr===numberOfPlayers && questionNr < 10){
+
+        //If last player, control the answers and go to next question and first player
+        controlAnswers(questionNr);
+        questionNr++;
+        playerNr=0;
     }
-    else{
 
-        if(playerNr===numberOfPlayers){
-            playerNr--;
+    //Check if not completed
+    if(questionNr < 10){
 
-            controlAnswers(questionNr);
-            questionNr++;
-            playerNr=0;
-        }
-
+        //Reset former player indicator
         let playerIndicators = Array.from(progressGrid.getElementsByClassName("player"));
         playerIndicators.forEach(function(indicator){
             if(indicator.value==="active"){
@@ -79,14 +91,16 @@ function setTurn(){
             }
         });
 
+        //Get next players indicator and set it active
         let playerIndicator = document.getElementById("player" + playerNr);
         playerIndicator.className = "player active";
         playerIndicator.value = "active";
 
+        //Set the question
         setUpQuestion();
+
+
     }
-
-
 
 }
 
@@ -94,17 +108,26 @@ function controlAnswers(questionNr){
 
     for(let i = 0; i < numberOfPlayers; i++){
 
+        //Get the progress bar segment element for the current question and each player
         let progSegment = document.getElementById("progSegment"+i+questionNr);
+        //Get the progress bar number element for each player
         let progressNumber = document.getElementById("progressNumber"+i);
+        //Get the current correct amount of answers from the JSON array for each player
         let correctAnswers = playerDataJSON.player[i].correct_answers;
 
+        //Check if player answer in array is the same as current correct answer.
         if(playerDataJSON.player[i].answers[questionNr].toLowerCase()===correctAnswer.toLowerCase()){
+
+            //Change progress bar segment to correct
             progSegment.className = "progSegment correct";
+
+            //Increase the amount of correct answers, add it to the JSON array and adjust the progress bar number
             correctAnswers++;
             playerDataJSON.player[i].correct_answers = correctAnswers;
             progressNumber.innerHTML = correctAnswers + "/10";
         }
         else{
+            //If wrong, set the progress bar segment to wrong
             progSegment.className = "progSegment wrong";
 
         }
@@ -113,50 +136,59 @@ function controlAnswers(questionNr){
 }
 
 async function setUpQuestion(){
+
+    //Check if first player
     if(playerNr===0){
+
+        //Get question data from JSON array
         questionType = quizDataJSON.results[questionNr].type;
         currentQuestion = quizDataJSON.results[questionNr].question;
         correctAnswer = quizDataJSON.results[questionNr].correct_answer;
         currentAnswers = Array.from(quizDataJSON.results[questionNr].incorrect_answers);
-    }
 
-    questionDisplay.innerHTML = currentQuestion;
+        //Show current question
+        questionDisplay.innerHTML = currentQuestion;
 
-    setUpAnswers().then(()=>{
+        setUpAnswers().then(()=>{
 
-        buildAnswerGrid().then(()=>{
+            buildAnswerGrid().then(()=>{
 
-            setAnswerButtonsListeners();
+                setAnswerButtonsListeners();
+
+            },(ErrorMessage)=>{
+                console.log(ErrorMessage);
+            });
 
         },(ErrorMessage)=>{
             console.log(ErrorMessage);
         });
 
-    },(ErrorMessage)=>{
-        console.log(ErrorMessage);
-    });
-
+    }
 }
 
 function setUpAnswers(){
 
     return new Promise((resolve, reject) => {
 
+        //Check if question type is boolean
         if(questionType==="boolean"){
+            //Set current answer array
             currentAnswers = ["True","False"];
             resolve();
         }
         else{
-            if(playerNr===0){
-                randomizeAnswerArray().then(()=> {
-                    resolve();
-                }, (ErrorMessage)=> {
-                    reject(ErrorMessage);
-                });
-            }
-            else{
+
+            randomizeAnswerArray().then(()=> {
+
                 resolve();
-            }
+
+                }, (error)=> {
+
+                    console.log(error.message)
+                });
+
+
+
         }
 
     })
@@ -192,7 +224,7 @@ function randomizeAnswerArray() {
                 resolve();
             }
             else{
-                reject("Error randomizing answers");
+                reject(new Error("Error randomizing answers"));
             }
         },500)
     });
@@ -203,20 +235,30 @@ function buildAnswerGrid() {
 
     return new Promise((resolve, reject) => {
 
+        //Delete former answers
         answerGrid.innerHTML="";
+
+        //Create a div element for each answer and add them to the grid
         let answerId = 0;
         currentAnswers.forEach(function (answer) {
-            let answerDiv = document.createElement("div");
-            answerDiv.className = "answer";
-            answerDiv.id = "answer" + answerId;
-            answerDiv.innerHTML = answer;
-            answerDiv.value = currentAnswers.indexOf(answer);
-            answerId++;
 
-            answerGrid.appendChild(answerDiv);
+            //Check if not an empty answer
+            if(answer.length>0){
+                let answerDiv = document.createElement("div");
+                answerDiv.className = "answer";
+                answerDiv.id = "answer" + answerId;
+                answerDiv.innerHTML = answer;
+
+                //The value is used to save the answer in the player data json array when the player locks the answer
+                answerDiv.value = currentAnswers.indexOf(answer);
+                answerId++;
+
+                answerGrid.appendChild(answerDiv);
+            }
         });
 
         setTimeout(()=>{
+            //Check that each answer is represented in the html
             if(answerGrid.querySelectorAll("div").length===currentAnswers.length){
                 resolve();
             }
@@ -241,8 +283,12 @@ function setAnswerButtonsListeners(){
 
 function answerButtonAction(number){
 
+    //Check all buttons
     answerButtons.forEach((button)=>{
+
+        //If the Id matches the button that is clicked
         if(button.getAttribute("id") === "answer" + number){
+            //Set it to selected and use the value to get the current answer from the quiz data to the answer data
             button.className = "answer selected";
             playerDataJSON.player[playerNr].answers[questionNr] = currentAnswers[button.value];
         }
@@ -253,20 +299,17 @@ function answerButtonAction(number){
 }
 
 function lockAnswer(){
-    //remove the event listener to prevent double click
+    //Remove the event listener to prevent double click
     lockAnswerButton.removeEventListener("click",lockAnswer);
 
-    //after 500 ms..
     setTimeout(()=>{
-        //reset the event listener
+        //Reset the event listener
         lockAnswerButton.addEventListener("click",lockAnswer);
 
-        //switch to next players turn
+        //Switch to next players turn
         playerNr++;
         setTurn();
     }, 500);
 }
 
-function endQuiz(){
-
-}
+initializeQuiz();
